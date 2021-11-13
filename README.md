12b648c55d8f34db6b6e75857d996b024a217143
peterjinchristiansen <peterjinchristiansen@gmail.com>
2021-11-13 15:49:11 -0700

    :(

--- README.md
+++
@@ -1,5 +0,0 @@
-# Study Groups
-## Description
-This is an application designed to help students collaborate with one another
-
----
--- client/src/pages/One.js
+++
@@ -1,9 +0,0 @@
-const One = () => {
-    return(
-        <div>
-            <h1>One Page</h1>
-        </div>
-    )
-}
-
-export default One
